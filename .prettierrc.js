427be6555dfb3006307e07e9eb32a5e33220c1bc
SamSayyed26 <sumayyah.sayyed@focusteck.com>
2023-11-23 17:32:18 +0500

    Initial Commit

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  trailingComma: 'es5',
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+}
---
+++ src/app/cost-estimation-calculator/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const page = () => {
+  return <div>COST ESTIMATION CALCULATOR</div>
+}
+
+export default page
